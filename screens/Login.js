0930c708a7bbea4461b49f062f2c8f8e87460b8e
Moili20 <122986067+Moili20@users.noreply.github.com>
2024-04-19 16:28:12 +0300

    Update Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -1,65 +1,68 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform } from 'react-native';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase/Config';
 import { useNavigation } from '@react-navigation/native';
 
 const Login = () => {
-    const navigation = useNavigation();
-    const [email, setEmail] = useState('testi9@foo.com');
-    const [password, setPassword] = useState('123456');
-    const [error, setError] = useState(null)
+  const navigation = useNavigation();
+  const [email, setEmail] = useState('testi9@foo.com');
+  const [password, setPassword] = useState('123456');
+  const [error, setError] = useState(null)
 
-    const dismissKeyboard = () => {
-      Keyboard.dismiss();
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  }
+
+  const onLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigation.replace('Home');
+    } catch (error) {
+      console.log(error);
+      let errorMessage = 'Incorrect credentials';
+      setError(errorMessage);
     }
-
-    const onLogin = async () => {
-      try {
-        await signInWithEmailAndPassword(auth, email, password);
-        navigation.replace('Home');
-      } catch (error) {
-        console.log(error);
-        let errorMessage = 'Incorrect credentials';
-        setError(errorMessage); 
-      }
   };
 
-    return (
-      <TouchableWithoutFeedback onPress={dismissKeyboard}>
-        <KeyboardAvoidingView 
-          style={styles.container}
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
-        >
-            <Image
-        source={require('../assets/smartsaver_logo.png')}
-        style={styles.logo}
-      />
-            <Text style={styles.header}>SmartSaver</Text>
-            <TextInput
-                style={styles.input}
-                value={email}
-                onChangeText={setEmail}
-                placeholder="Email address"
-                autoCapitalize="none"
-            />
-            <TextInput
-                style={styles.input}
-                value={password}
-                onChangeText={setPassword}
-                placeholder="Password"
-                secureTextEntry
-            />
-            {error && <Text style={styles.error}>{error}</Text>}
-            <TouchableOpacity style={styles.button} onPress={onLogin}>
-                <Text style={styles.buttonText}>Login</Text>
-            </TouchableOpacity>
-            <Text onPress={() => navigation.navigate('Signup')} style={styles.signUpLink}>
-                No account yet? Sign up
-            </Text>
-        </KeyboardAvoidingView>
-      </TouchableWithoutFeedback>
-    );
+  return (
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <Image
+          source={require('../assets/smartsaver_logo.png')}
+          style={styles.logo}
+        />
+        <Text style={styles.header}>SmartSaver</Text>
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          placeholder="Email address"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          placeholder="Password"
+          secureTextEntry
+        />
+        {error && <Text style={styles.error}>{error}</Text>}
+        <TouchableOpacity style={styles.button} onPress={onLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <Text style={styles.signUpText}>
+          No account yet? {' '}
+          <Text onPress={() => navigation.navigate('Signup')} style={styles.signUpLink}>
+            Sign up
+          </Text>
+        </Text>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -97,6 +100,9 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  signUpText: {
+    marginTop: 20,
   },
   signUpLink: {
     marginTop: 20,
