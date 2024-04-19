23bb99650d8a7415c650f4c827d1511145567be4
Veeko23 <122440628+Veeko23@users.noreply.github.com>
2024-04-19 17:50:38 +0300

    Transactions Update
    Updated UI and made it more user friendly.

--- screens/addSavings.js
+++ screens/addSavings.js
@@ -125,7 +125,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 16,
-    backgroundColor: '#EF2125',
+    backgroundColor: 'red',
   },
   setSavingsText: {
     color: 'white',
