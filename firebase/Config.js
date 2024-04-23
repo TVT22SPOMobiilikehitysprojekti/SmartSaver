56532323b78002381bb0269921cec0d37e27ac17
Veeti <122440628+Veeko23@users.noreply.github.com>
2024-04-23 11:58:57 +0300

    Update Config.js

--- firebase/Config.js
+++ firebase/Config.js
@@ -6,12 +6,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC3PfQrx8Rp3cvAFg_clwE3HNwAKIZsB1Q",
-    authDomain: "smartsaver-6b7d4.firebaseapp.com",
-    projectId: "smartsaver-6b7d4",
-    storageBucket: "smartsaver-6b7d4.appspot.com",
-    messagingSenderId: "1055547833369",
-    appId: "1:1055547833369:web:0224daf600520d45c67e88"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase App
